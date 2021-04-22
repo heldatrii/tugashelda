e46337a284dbaf169b6c5bbef7f1eb64d31092fc
heldatrii <82790937+heldatrii@users.noreply.github.com>
2021-04-22 16:42:53 +0700

    main program dan kelas siswa
    kelas siswa

---
+++ tugas oop/Siswa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas1
+{
+    class Siswa
+    {
+        public string Nama { get; set; }
+        public int Nuts { get; set; }
+        public int Nuas { get; set; }
+        public int Ntugas { get; set; }
+
+
+        public Siswa(string nama, int nuas, int nuts, int ntugas) 
+        {
+            Nama = nama;
+            Nuts = nuts;
+            Nuas = nuas;
+            Ntugas = ntugas;                     
+        }
+
+      }
+}
