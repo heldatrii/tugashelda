4cf43775b7fe69150777f6a73849d38eac4952d3
heldatrii <82790937+heldatrii@users.noreply.github.com>
2021-05-01 21:05:47 +0700

    syntax untuk my context

---
+++ Context/MyContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserManagement.Models;
+
+namespace UserManagement.Context
+{
+    public class MyContext : DbContext 
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options) 
+        {  }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<University> Universities { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Profiling> Profilings { get; set; }
+        public DbSet<Education> Educations { get; set; }
+    
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableSensitiveDataLogging();
+            optionsBuilder.UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+
+    
+}
+
+
+
+
+
+
+
